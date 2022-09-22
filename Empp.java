482a4b752f305590f72468e4b527f78999833bdf
imranayesha18 <80262315+imranayesha18@users.noreply.github.com>
2022-09-22 10:46:53 +0530

    Add files via upload

---
+++ Empp.java
@@ -0,0 +1,50 @@
+package org.example;
+    class Emp
+    {
+        int id;
+        String name, address;
+        String phone;
+        public Emp(int i, String n, String a, String p)
+        {
+            this.id = i;
+            this.name = n;
+            this.address = a;
+            this.phone = p;
+        }
+        public void display(int i)
+        {
+            System.out.println("Name:"+id+"\nphone:"+phone);
+        }
+        public void display()
+        {
+            System.out.println("\nName:"+name+"\nAddress:"+address);
+        }
+    }
+    class HR extends Emp
+    {
+        int salary;
+        public HR(int i, String n, String a, String p, int sal)
+        {
+            super(i, n, a, p); this.salary = sal;
+        }
+        public void empWithSalary()
+        {
+            System.out.println("\nEmployee Name: "+name+"\nSalary: "+salary);
+        }
+        public void empWithSalary(int tax)
+        {
+            System.out.println("\nEmployee Name: "+name+"\nSalary: "+(salary-tax)+"\nTax Deducted: "+tax);
+        }
+    }
+    class Empp
+    {
+        public static void main(String args[])
+        {
+            Emp obj1 = new Emp(1,"Ayesha","Imran", "0123456789");
+            HR obj2 = new HR(1,"Ayesha","iMRAN", "0123456789", 25000);
+            obj1.display(1);
+            obj1.display();
+            obj2.empWithSalary();
+            obj2.empWithSalary(3000);
+        }
+    }
