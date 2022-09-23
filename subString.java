b364abcdc6c789261fd13472833fad39cc839f98
imranayesha18 <80262315+imranayesha18@users.noreply.github.com>
2022-09-23 16:41:01 +0530

    Add files via upload

---
+++ subString.java
@@ -0,0 +1,9 @@
+public class New {
+    public static void main(String[] args) {
+        String s1 = "I am Ayesha and joined Amantya on 01-09-22";
+        int fi=s1.lastIndexOf(" ");
+        System.out.println(s1.substring(fi, fi+5));
+
+    }
+}
+
