eb0414310df2cc027a784bc49dc83db15309633b
imranayesha18 <80262315+imranayesha18@users.noreply.github.com>
2022-10-06 14:51:43 +0530

    Add files via upload

---
+++ NewChar.java
@@ -0,0 +1,11 @@
+public class NewChar {
+    public static void main(String args[]) {
+        String name = "Ayesha";
+        String rev = "";
+        int len =name.length();
+        for (int i = len - 1; i >= 0; i--) {
+            rev = rev + name.charAt(i);
+        }
+        System.out.println("reverse of "+name+" is "+rev);
+    }
+}
