527a4993be158cca35d4903e547e54946ef9a389
imranayesha18 <80262315+imranayesha18@users.noreply.github.com>
2022-10-06 12:56:23 +0530

    Delete subString.java

--- subString.java
+++
@@ -1,9 +0,0 @@
-public class New {
-    public static void main(String[] args) {
-        String s1 = "I am Ayesha and joined Amantya on 01-09-22";
-        int fi=s1.lastIndexOf(" ");
-        System.out.println(s1.substring(fi, fi+5));
-
-    }
-}
-
